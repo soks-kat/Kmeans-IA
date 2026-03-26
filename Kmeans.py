2a50288d00493bb1c168480ed4409ac591257f13
soks-kat <marinacatalan12@gmail.com>
2026-03-26 15:19:56 +0100

    initial commit

---
+++ Kmeans.py
@@ -0,0 +1,208 @@
+__authors__ = 'Kai Catalan Daniels, Daniel Hurtado Labandeira'
+__group__ = '07'
+
+from math import sqrt
+from typing import TypedDict
+import numpy as np
+import numpy.typing as npt
+import utils
+
+class Options(TypedDict):
+    km_init: str
+    verbose: bool
+    tolerance: int
+    max_iter: int
+    fitting: str
+
+class KMeans:
+
+    def __init__(self, X: npt.NDArray[np.float64], K: int = 1, options: Options = None) -> None:
+        """
+         Constructor of KMeans class
+             Args:
+                 K (int): Number of cluster
+                 options (dict): dictionary with options
+            """
+        self.num_iter: int = 0
+        self.K: int = K
+        self._init_X(X)
+        self._init_options(options)  # DICT options
+
+    #############################################################
+    ##  THIS FUNCTION CAN BE MODIFIED FROM THIS POINT, if needed
+    #############################################################
+
+    def _init_X(self, X: npt.NDArray[np.float64]) -> None:
+        """Initialization of all pixels, sets X as an array of data in vector form (PxD)
+            Args:
+                X (list or np.array): list(matrix) of all pixel values
+                    if matrix has more than 2 dimensions, the dimensionality of the sample space is the length of
+                    the last dimension
+        """
+        if X.dtype != np.float64:
+            X = X.astype('float64')
+
+        if X.ndim != 2:
+            shape = X.shape
+            X = X.reshape(shape[0]*shape[1], shape[2])
+        #######################################################
+        ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+        ##  AND CHANGE FOR YOUR OWN CODE
+        #######################################################
+        self.X: npt.NDArray[np.float64] =  X
+
+    def _init_options(self, options: Options = None):
+        """
+        Initialization of options in case some fields are left undefined
+        Args:
+            options (dict): dictionary with options
+        """
+        if options is None:
+            options = {}
+        if 'km_init' not in options:
+            options['km_init'] = 'first'
+        if 'verbose' not in options:
+            options['verbose'] = False
+        if 'tolerance' not in options:
+            options['tolerance'] = 0
+        if 'max_iter' not in options:
+            options['max_iter'] = np.inf
+        if 'fitting' not in options:
+            options['fitting'] = 'WCD'  # within class distance.
+
+        # If your methods need any other parameter you can add it to the options dictionary
+        self.options: Options = options
+
+        #############################################################
+        ##  THIS FUNCTION CAN BE MODIFIED FROM THIS POINT, if needed
+        #############################################################
+
+    def _init_centroids(self):
+        """
+        Initialization of centroids
+        """
+
+        #######################################################
+        ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+        ##  AND CHANGE FOR YOUR OWN CODE
+        #######################################################
+        if self.options['km_init'].lower() == 'first':
+            self.centroids: npt.NDArray[np.float64] = np.random.rand(self.K, self.X.shape[1])
+            self.old_centroids: npt.NDArray[np.float64] = np.random.rand(self.K, self.X.shape[1])
+        else:
+            self.centroids = np.random.rand(self.K, self.X.shape[1])
+            self.old_centroids = np.random.rand(self.K, self.X.shape[1])
+
+    def get_labels(self):
+        """
+        Calculates the closest centroid of all points in X and assigns each point to the closest centroid
+        """
+        #######################################################
+        ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+        ##  AND CHANGE FOR YOUR OWN CODE
+        #######################################################
+        distances = distance(self.X, self.centroids) # [N x K]
+        # np.random.randint(self.K, size=self.X.shape[0])
+        self.labels: npt.NDArray[np.int64] = np.argmax(distances, axis=0) # [N]
+
+    def get_centroids(self):
+        """
+        Calculates coordinates of centroids based on the coordinates of all the points assigned to the centroid
+        """
+        #######################################################
+        ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+        ##  AND CHANGE FOR YOUR OWN CODE
+        #######################################################
+        self.old_centroids = self.centroids
+        
+        pass
+
+    def converges(self, error: float = 0) -> np.bool:
+        """
+        Checks if there is a difference between current and old centroids
+        """
+        #######################################################
+        ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+        ##  AND CHANGE FOR YOUR OWN CODE
+        #######################################################
+        diffs = np.subtract(self.centroids, self.old_centroids)
+        return np.all(diffs <= error)
+
+    def fit(self):
+        """
+        Runs K-Means algorithm until it converges or until the number of iterations is smaller
+        than the maximum number of iterations.
+        """
+        #######################################################
+        ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+        ##  AND CHANGE FOR YOUR OWN CODE
+        #######################################################
+        pass
+
+    def withinClassDistance(self) -> float:
+        """
+         returns the within class distance of the current clustering
+        """
+
+        #######################################################
+        ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+        ##  AND CHANGE FOR YOUR OWN CODE
+        #######################################################
+        pass
+
+    def find_bestK(self, max_K: int) -> int:
+        """
+         sets the best k analysing the results up to 'max_K' clusters
+        """
+        #######################################################
+        ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+        ##  AND CHANGE FOR YOUR OWN CODE
+        #######################################################
+        pass
+
+
+def distance(X: npt.NDArray[np.float64], C: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    """
+    Calculates the distance between each pixel and each centroid
+    Args:
+        X (numpy array): PxD 1st set of data points (usually data points)
+        C (numpy array): KxD 2nd set of data points (usually cluster centroids points)
+
+    Returns:
+        dist: PxK numpy array position ij is the distance between the
+        i-th point of the first set an the j-th point of the second set
+    """
+
+    #########################################################
+    ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+    ##  AND CHANGE FOR YOUR OWN CODE
+    #########################################################
+
+    D = np.zeros((X.shape[0], C.shape[0]))
+
+    for x_id, x in X:
+        for c_id, c in C:
+            dist = 0
+            for i in X.shape[1]:
+                dist += (x[i] - c[i])**2
+            dist = sqrt(dist)
+            D[x_id][c_id] = dist
+
+    return D
+
+
+def get_colors(centroids: npt.NDArray[np.float64]) -> npt.NDArray[np.int64]:
+    """
+    for each row of the numpy matrix 'centroids' returns the color label following the 11 basic colors as a LIST
+    Args:
+        centroids (numpy array): KxD 1st set of data points (usually centroid points)
+
+    Returns:
+        labels: list of K labels corresponding to one of the 11 basic colors
+    """
+
+    #########################################################
+    ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+    ##  AND CHANGE FOR YOUR OWN CODE
+    #########################################################
+    return np.argmax(utils.get_color_prob(centroids), axis=0)
