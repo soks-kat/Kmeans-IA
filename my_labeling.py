04d9d6088e0ded41c99467f8e787013ad20f8ba0
CyanideData2 <danielhurtadolabandeira@gmail.com>
2026-03-29 19:46:24 +0200

    custom testing

--- my_labeling.py
+++ my_labeling.py
@@ -1,7 +1,9 @@
 __authors__ = 'TO_BE_FILLED'
 __group__ = 'TO_BE_FILLED'
 
+from Kmeans import KMeans, Options
 from utils_data import read_dataset, read_extended_dataset, crop_images
+import utils_data
 
 
 if __name__ == '__main__':
@@ -17,4 +19,19 @@ if __name__ == '__main__':
     imgs, class_labels, color_labels, upper, lower, background = read_extended_dataset()
     cropped_images = crop_images(imgs, upper, lower)
 
-    # You can start coding your functions here
+    
+    defaults: Options = {
+        "km_init": "random",
+        "verbose": False,
+        "tolerance": 0,
+        "opt_DEC": 0.8,
+        "max_iter": 100,
+        "fitting": "WCD",
+    }
+    first = imgs[0]
+    km = KMeans(first, 1, defaults)
+    km.find_bestK(10)
+    print("Best K = ", km.K)
+    km.fit()
+    utils_data.visualize_k_means(km, first.shape)
+
